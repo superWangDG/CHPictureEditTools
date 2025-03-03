5ad178bb5aa02324430caa4e2a687bee6734bba6
wangdegui <huangqiupeng@cloudhearing.cn>
2025-03-03 10:26:17 +0800

    提交代码

--- CHPictureEditTools/Sources/CHTestTools.swift
+++ CHPictureEditTools/Sources/CHTestTools.swift
@@ -5,7 +5,10 @@
 //  Created by apple on 2025/2/28.
 //
 
-public class CHTestTools {
+public class CHTestTools: NSObject {
+    public override init() {
+        super.init()
+    }
     public func printTest() {
         print("输出了测试的信息")
     }
